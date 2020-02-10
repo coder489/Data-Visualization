4494b78a42ac3dd26ba7bd7168d145172720de37
coder489 <aai34@georgetown.edu>
2020-02-10 17:50:42 -0500

    Pie Chart

--- three_charts.py
+++ three_charts.py
@@ -1,36 +1,55 @@
 import plotly
 import plotly.graph_objs as go
 
+import plotly
+import plotly.graph_objs as go
+
 
 ## CHART 2 (LINE CHART) ##
 
-line_data = [
-   {"date": "2019-01-01", "stock_price_usd": 100.00},
-   {"date": "2019-01-02", "stock_price_usd": 101.01},
-   {"date": "2019-01-03", "stock_price_usd": 120.20},
-   {"date": "2019-01-04", "stock_price_usd": 107.07},
-   {"date": "2019-01-05", "stock_price_usd": 142.42},
-   {"date": "2019-01-06", "stock_price_usd": 135.35},
-   {"date": "2019-01-07", "stock_price_usd": 160.60},
-   {"date": "2019-01-08", "stock_price_usd": 162.62},
-]
+#line_data = [
+#   {"date": "2019-01-01", "stock_price_usd": 100.00},
+#   {"date": "2019-01-02", "stock_price_usd": 101.01},
+#   {"date": "2019-01-03", "stock_price_usd": 120.20},
+#   {"date": "2019-01-04", "stock_price_usd": 107.07},
+#   {"date": "2019-01-05", "stock_price_usd": 142.42},
+#   {"date": "2019-01-06", "stock_price_usd": 135.35},
+#   {"date": "2019-01-07", "stock_price_usd": 160.60},
+#   {"date": "2019-01-08", "stock_price_usd": 162.62},
+#]
+#
+#
+#date_list = [x["date"] for x in line_data]
+#
+#price_list = [y["stock_price_usd"] for y in line_data]
+#
+#
+#
+#plotly.offline.plot({
+#    "data": [go.Scatter(x=date_list, y=price_list)],
+#    "layout": go.Layout(title="Stock Price Over Time")
+#}, auto_open=True)
 #
 #print("----------------")
 #print("GENERATING LINE GRAPH...")
-#print(line_data) # TODO: create a line graph based on the line_data
-
-date_list = [x["date"] for x in line_data]
-
-price_list = [y["stock_price_usd"] for y in line_data]
 
 
+## PIE CHART ##
 
-plotly.offline.plot({
-    "data": [go.Scatter(x=date_list, y=price_list)],
-    "layout": go.Layout(title="Stock Price Over Time")
-}, auto_open=True)
+pie_data = [
+    {"company": "Company X", "market_share": 0.55},
+    {"company": "Company Y", "market_share": 0.30},
+    {"company": "Company Z", "market_share": 0.15}
+]
+
+company_list = [x["company"] for x in pie_data]
+market_amount = [y["market_share"] for y in pie_data]
+
+
+trace = go.Pie(labels=company_list, values=market_amount)
+
+plotly.offline.plot([trace], filename="basic_pie_chart.html", auto_open=True)
 
 print("----------------")
-print("GENERATING LINE GRAPH...")
-
-
+print("GENERATING PIE CHART...")
+print(pie_data) 
